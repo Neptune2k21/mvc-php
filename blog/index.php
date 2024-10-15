beac89707e00780379b41ce4f187757053ae5c72
Neptune2k21 <116343729+Neptune2k21@users.noreply.github.com>
2024-10-15 08:57:44 +0200

    Isolez l'affichage du traitement PHP

--- blog/index.php
+++ blog/index.php
@@ -22,40 +22,7 @@ while ($row = $statement->fetch()) {
     ];
     $posts[] = $post;
 }
+require('templates/homepage.php');
 
 $statement->closeCursor(); // On ferme le curseur après avoir récupéré les données
 ?>
-<!DOCTYPE html>
-<html>
-   <head>
-      <meta charset="utf-8" />
-      <title>Le blog de l'AVBN</title>
-      <link href="style.css" rel="stylesheet" />
-   </head>
-
-   <body>
-      <h1>Le super blog de l'AVBN !</h1>
-      <p>Derniers billets du blog :</p>
-
-      <?php
-      foreach ($posts as $post) {
-      ?>
-      <div class="news">
-         <h3>
-            <?php echo htmlspecialchars($post['title']); ?>
-            <em>le <?php echo $post['frenchCreationDate']; ?></em>
-         </h3>
-         <p>
-         <?php
-         // On affiche le contenu du billet
-         echo nl2br(htmlspecialchars($post['content']));
-         ?>
-         <br />
-         <em><a href="#">Commentaires</a></em>
-         </p>
-      </div>
-      <?php
-      } // Fin de la boucle des billets
-      ?>
-   </body>
-</html>
